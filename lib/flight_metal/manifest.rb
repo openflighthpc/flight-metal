4de8b5de3d4c31cd6566111a67165374fb9f5843
william <william.mccumstie@gmail.com>
2019-06-06 15:31:05 +0100

    Define the basic structure of the Manifest file

---
+++ lib/flight_metal/manifest.rb
@@ -0,0 +1,80 @@
+# frozen_string_literal: true
+
+#==============================================================================
+# Copyright (C) 2019-present Alces Flight Ltd.
+#
+# This file is part of flight-metal.
+#
+# This program and the accompanying materials are made available under
+# the terms of the Eclipse Public License 2.0 which is available at
+# <https://www.eclipse.org/legal/epl-2.0>, or alternative license
+# terms made available by Alces Flight Ltd - please direct inquiries
+# about licensing to licensing@alces-flight.com.
+#
+# This project is distributed in the hope that it will be useful, but
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, EITHER EXPRESS OR
+# IMPLIED INCLUDING, WITHOUT LIMITATION, ANY WARRANTIES OR CONDITIONS
+# OF TITLE, NON-INFRINGEMENT, MERCHANTABILITY OR FITNESS FOR A
+# PARTICULAR PURPOSE. See the Eclipse Public License 2.0 for more
+# details.
+#
+# You should have received a copy of the Eclipse Public License 2.0
+# along with this project. If not, see:
+#
+#  https://opensource.org/licenses/EPL-2.0
+#
+# For more information on flight-account, please visit:
+# https://github.com/alces-software/flight-metal
+#===============================================================================
+
+require 'hashie'
+require 'pathname'
+
+require 'active_support/concern'
+
+module FlightMetal
+  class Manifest < Hashie::Trash
+    include Hashie::Extensions::Dash::Coercion
+    include Hashie::Extensions::IndifferentAccess
+
+    def self.load(base)
+      path = File.join(base, 'manifest.yaml')
+      data = YAML.safe_load(File.read(path)).to_h
+      Manifests::Base.new(data)
+    end
+  end
+
+  module Manifests
+    class Domain < Manifest
+      property :name
+      property :bmc_username
+      property :bmc_password
+      property :gateway_ip
+    end
+
+    class Group < Manifest
+      property :name
+    end
+
+    class Node < Manifest
+      property :name
+      property :build_ip
+      property :gateway_ip
+      property :bmc_ip
+      property :bmc_username
+      property :bmc_password
+      property :primary_group
+      property :secondary_groups, coerce: Array
+      property :kickstart, coerce: Pathname
+      property :pxelinux, coerce: Pathname
+      property :aws, coerce: Pathname
+      property :azure, coerce: Pathname
+    end
+
+    class Base < Manifest
+      property :domain, default: {}, coerce: Domain
+      property :groups, default: [], coerce: Array[Group]
+      property :nodes,  default: [], coerce: Array[Node]
+    end
+  end
+end
